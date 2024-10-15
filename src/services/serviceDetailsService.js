ffbc909312120be5a3feec584cf20ad8247752b3
pavanpiyumantha44 <pavanpiyumantha222@gmail.com>
2024-10-16 00:29:06 +0530

    Added url

--- src/services/serviceDetailsService.js
+++ src/services/serviceDetailsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://booking-app-backend-pearl.vercel.app';
+const BASE_URL = 'http://localhost:5000';
 
 const getServiceDetails = async(id)=>{
     try {
